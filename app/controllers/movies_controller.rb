876fbf878a5b54ea94ae73ab6e1e97362979133b
Aldo Portillo <94483081+aldoportillo@users.noreply.github.com>
2023-09-28 21:37:51 +0000

    Can modify movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,54 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch("path_id")
+    @the_id = params.fetch("path_id")
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where({ :id => @the_id })
     @the_movie = matching_movies.at(0)
 
     render({ :template => "movie_templates/show" })
   end
+
+
+  def create
+    movie = Movie.new
+
+    movie.title = params.fetch("query_title")
+    movie.year = params.fetch("query_year")
+    movie.duration = params.fetch("query_duration")
+    movie.description = params.fetch("query_description")
+    movie.image = params.fetch("query_image")
+    movie.director_id = params.fetch("query_director_id")
+
+    movie.save
+  end
+
+  def update
+
+    id = params.fetch("path_id")
+
+    @movie = Movie.where({:id => id}).first
+
+    @movie.title = params.fetch("query_title")
+    @movie.year = params.fetch("query_year")
+    @movie.duration = params.fetch("query_duration")
+    @movie.description = params.fetch("query_description")
+    @movie.image = params.fetch("query_image")
+    @movie.director_id = params.fetch("query_director_id")
+
+    @movie.save
+
+    redirect_to("/movies/#{id}")
+
+  end
+
+  def delete
+
+    id = params.fetch("path_id")
+
+    Movie.destroy(id)
+
+    redirect_to("/movies")
+  end
+
 end
